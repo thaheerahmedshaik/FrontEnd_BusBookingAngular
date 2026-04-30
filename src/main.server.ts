19430ffc5deee009031837fdfc891ea914cef55f
thaheerahmedshaik <thaheerahmeds@gmail.com>
2026-04-30 23:36:32 +0530

    updated

--- src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config.server';
-
-export default function bootstrap() {
-  return bootstrapApplication(AppComponent, appConfig);
-}
